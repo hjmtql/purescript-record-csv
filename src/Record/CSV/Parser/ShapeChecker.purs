1238a4128863d1fd490e4e25bb07fe4c1e01525b
hjmtql <hjmtql@gmail.com>
2020-06-05 11:31:43 +0900

    refactor ShapeChecker

--- src/Record/CSV/Parser/ShapeChecker.purs
+++ src/Record/CSV/Parser/ShapeChecker.purs
@@ -11,12 +11,6 @@ import Record.CSV.Type (CSV, CSVResult)
 sameLength :: CSV -> CSVResult CSV
 sameLength L.Nil = Right L.Nil
 
-sameLength (L.Cons h tail) = L.Cons h <$> check tail
-  where
-  len = L.length h
-
-  check L.Nil = Right L.Nil
-
-  check (L.Cons x xs)
-    | L.length x == len = L.Cons <$> Right x <*> check xs
-    | otherwise = Left $ DifferntColumnLength "Column lengths are not the same."
+sameLength a@(L.Cons h tail)
+  | L.all (eq (L.length h)) (map L.length tail) = Right a
+  | otherwise = Left $ DifferntColumnLength "Column lengths are not the same."
